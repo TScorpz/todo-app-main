8f7a81af99b6b64860452d3849ed1496e2e8be6c
TScorpz <shamaner88@gmail.com>
2021-09-29 16:48:48 +0300

    Task marking and deletion.
    Done the checkboxes.
    Added hover effects for filters.
    Added an array to track completed tasks.
    App now displays how many active tasks are left.
    Added 'Clear Completed' button.

--- src/todoList.js
+++ src/todoList.js
@@ -7,7 +7,8 @@ export default class TodoList extends Component {
 
         this.state = {
             userInput: '',
-            list: []
+            list: [],
+            doneTasks: []
         }
     }
 
@@ -25,25 +26,63 @@ export default class TodoList extends Component {
 
     addToList() {
         let listArray = this.state.list;
+        let doneList = this.state.doneTasks;
         listArray.push(this.state.userInput);
+        doneList.push(false);
 
         this.setState({
             list: listArray,
+            doneTasks: doneList,
             userInput: ''
         })
     }
 
+    toggleCheckbox(i) {
+        let doneList = this.state.doneTasks;
+        doneList[i] = !doneList[i];
+
+        this.setState({
+            doneTasks: doneList
+        })
+    }
+
+    tasksLeft() {
+        let count = 0;
+
+        this.state.doneTasks.forEach(element => {
+            if (!element) {
+                count++;
+            }
+        });
+
+        return count;
+    }
+
     deleteHandler = (i) => {
+        let doneList = this.state.doneTasks;
         let todoList = this.state.list;
+        doneList.splice(i, 1);
         todoList.splice(i, 1);
-        this.setState({ list: todoList });
+        this.setState({
+            doneTasks: doneList,
+            list: todoList
+        });
+    }
+
+    clearCompleted() {
+        for (let i = 0; i < this.state.doneTasks.length; i++) {
+            if (this.state.doneTasks[i]) {
+                this.deleteHandler(i);
+                i--;
+            }
+        }
     }
 
     render() {
         return (
             <>
                 <div className="newTodo">
-                    <div className='check'></div>
+                    <div className='checkbox'></div>
                     <input
                         onKeyPress={(e) => this.handleInput(e)}
                         onChange={(e) => this.handleChange(e)}
@@ -55,25 +94,30 @@ export default class TodoList extends Component {
 
                 <div className="list">
                     <ul>
-                        {this.state.list.map((val, i) =>
+                        {this.state.list.map((val, i) => {
                             <li key={i}>
                                 <div className="task">
-                                    <div className='check'></div>
-                                    <p>{val}</p>
+                                    <input type="checkbox" name="check" id={'check-' + i} className='checkbox' onChange={() => this.toggleCheckbox(i)} checked={this.state.doneTasks[i]} />
+                                    <label htmlFor={'check-' + i} className='lbl-checkbox checkbox'></label>
+                                    <label htmlFor={'check-' + i} className='lbl-value'>{val}</label>
                                     <div className="close" onClick={e => this.deleteHandler(i)}></div>
                                 </div>
                             </li>
+                        }
                         )}
                     </ul>
                 </div>
 
+
                 <div className="filters">
-                    <input type='radio' name='filter' id='All' className='all' value='All' />
+                    <div className='items-left'>{this.tasksLeft()} items left</div>
+                    <input type='radio' name='filter' id='All' className='all' value='All' defaultChecked />
                     <label htmlFor="All">All</label>
                     <input type='radio' name='filter' id='Active' className='active' value='Active' />
                     <label htmlFor="Active">Active</label>
                     <input type='radio' name='filter' id='Completed' className='completed' value='Completed' />
                     <label htmlFor="Completed">Completed</label>
+                    <div className='btn-clear-completed' onClick={() => this.clearCompleted()}>Clear Completed</div>
                 </div>
             </>
         );
